b5dd36702983d420fe76f3883483981ae99882ac
Yevhen Vriashnyk <jackavryashnik@gmail.com>
2024-03-16 02:05:37 +0200

    create MoviePage component

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,5 +1,66 @@
+import { useEffect, useState } from 'react';
+import { Formik, Field, Form } from 'formik';
+import fetchData from '../api';
+import { Link, useLocation, useSearchParams } from 'react-router-dom';
+
 const MoviesPage = () => {
-  return <div>MoviesPage</div>;
+  const [movies, setMovies] = useState([]);
+  const [query, setQuery] = useState('');
+  const [searchParams, setSearchParams] = useSearchParams();
+  const location = useLocation();
+
+  const handleSubmit = e => {
+    const inputVale = e.query;
+    setQuery(inputVale);
+    setSearchParams({ query: inputVale });
+  };
+
+  useEffect(() => {
+    if (searchParams.size && searchParams.get('query') !== query) {
+      setQuery(searchParams.get('query'));
+    }
+    if (!query) return;
+
+    async function fetchedData() {
+      try {
+        const data = await fetchData('search/movie', query);
+        setMovies(data.results);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchedData();
+  }, [query, searchParams]);
+
+  return (
+    <>
+      <Formik
+        initialValues={{
+          query: '',
+        }}
+        onSubmit={handleSubmit}
+      >
+        <Form>
+          <Field name="query" />
+          <button type="submit">Search</button>
+        </Form>
+      </Formik>
+
+      <ul>
+        {movies.length > 0 &&
+          movies.map(el => {
+            return (
+              <li key={el.id}>
+                <Link to={`/movies/${el.id}`} state={{ from: location }}>
+                  {el.title}
+                </Link>
+              </li>
+            );
+          })}
+      </ul>
+    </>
+  );
 };
 
 export default MoviesPage;
