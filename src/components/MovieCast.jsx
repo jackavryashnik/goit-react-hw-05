868bcd0db250d07acb7b181e9aee4495b34c8607
Yevhen Vriashnyk <jackavryashnik@gmail.com>
2024-03-16 21:51:57 +0200

    create route components

--- src/components/MovieCast.jsx
+++ src/components/MovieCast.jsx
@@ -1,5 +1,29 @@
+import { useEffect, useState } from 'react';
+import { fetchMovieData } from '../api';
+import { useParams } from 'react-router-dom';
+
 const MovieCast = () => {
-  return <div>MovieCast</div>;
+  const [cast, setCast] = useState('');
+  const { movieId } = useParams();
+
+  useEffect(() => {
+    async function fetchedData() {
+      try {
+        const data = await fetchMovieData(movieId);
+        setCast(data.results);
+      } catch (error) {
+        console.log('error');
+      }
+    }
+
+    fetchedData();
+  }, [movieId]);
+
+  return (
+    <>
+      <p>{cast}</p>
+    </>
+  );
 };
 
 export default MovieCast;
--- src/components/MovieReviews.jsx
+++ src/components/MovieReviews.jsx
@@ -1,5 +1,32 @@
+import { useEffect, useState } from 'react';
+import { fetchMovieReviews } from '../api';
+import { useParams } from 'react-router-dom';
+
 const MovieReviews = () => {
-  return <div>MovieReviews</div>;
+  const [review, setReview] = useState('');
+  const { movieId } = useParams();
+
+  useEffect(() => {
+    async function fetchedData() {
+      try {
+        const data = await fetchMovieReviews(movieId);
+        setReview(data.results);
+      } catch (error) {
+        console.log('error');
+      }
+    }
+
+    fetchedData();
+  }, [movieId]);
+
+  console.log(review);
+
+  return (
+    <>
+      <p>MovieReviews</p>
+      <p>{review}</p>
+    </>
+  );
 };
 
 export default MovieReviews;
